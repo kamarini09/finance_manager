95753b1171ec9031e0dd8b75c1c0d01fa68a9ed6
kamarini09 <moragiannikam@gmail.com>
2025-03-05 12:36:39 +0100

    start changing

--- category/categoryService.ts
+++ category/categoryService.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://169.254.218.5:3000/categories";
+const API_URL = "http://10.59.130.49:3000/categories";
 
 // fetch all categories
 export const fetchCategories = async () => {
--- navigation/TabNavigation.tsx
+++ navigation/TabNavigation.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import StackNavigator from "./StackNavigation";
 import NewScreen from "../screens/NewScreen";
+import Category from "../category/Category";
 
 export type HomeTabParamList = {
-  List: undefined;
-  New: undefined;
+  Entries: undefined;
+  Categories: undefined;
 };
 
 const Tab = createBottomTabNavigator<HomeTabParamList>();
@@ -13,8 +14,8 @@ const Tab = createBottomTabNavigator<HomeTabParamList>();
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="List" component={StackNavigator} />
-      <Tab.Screen name="New" component={NewScreen} />
+      <Tab.Screen name="Entries" component={StackNavigator} />
+      <Tab.Screen name="Categories" component={Category} />
     </Tab.Navigator>
   );
 }
