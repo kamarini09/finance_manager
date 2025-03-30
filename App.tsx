dd9accd7d24c60dc90ad0d492b6ca628d5252e6d
kamarini09 <moragiannikam@gmail.com>
2025-03-30 18:15:03 +0200

    change folder structure

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import TabNavigator from "./navigation/TabNavigation";
+import TabNavigator from "./src/navigation/TabNavigation";
 
 export default function App() {
   return (
